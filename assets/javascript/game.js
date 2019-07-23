faca8d5a51fb3897c85a885dac6d836213b8b26f
Matt Joseph <mattomjoe@yahoo.com>
2019-07-22 23:48:03 -0500

    add images, CSS

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+
+
+// Function to generate random number  
+function randomNumber(min, max) {  
+    return Math.floor(Math.random() * (max - min) + min); 
+}
+  
+// Function call 
+$("#random-num-gen").html("<h2>" + ( randomNumber(19, 120) + "</h2>");
