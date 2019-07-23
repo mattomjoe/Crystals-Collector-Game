65e19e39e13fc844746e829da5af78c74a51937e
Matt Joseph <mattomjoe@yahoo.com>
2019-07-23 17:53:38 -0500

    minor change to JS file (commment out script)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,27 @@
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Function to generate random number  
-function randomNumber(min, max) {  
-    return Math.floor(Math.random() * (max - min) + min); 
-}
+//function randomNumber(min, max) {  
+    //return Math.floor(Math.random() * (max - min) + min); 
+//}
   
 // Function call 
-$("#random-num-gen").html("<h2>" + ( randomNumber(19, 120) + "</h2>");
+//$("#random-num-gen").html("<h2>" + ( randomNumber(19, 120) + "</h2>");
