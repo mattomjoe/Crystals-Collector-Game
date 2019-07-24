983751b04840b9ba58ad46cc1ce35d4193773381
Matt Joseph <mattomjoe@yahoo.com>
2019-07-24 00:59:01 -0500

    move JS/jQ links in index.html, add vars and click functions to JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,61 @@
-// Script based on YouTube video https://www.youtube.com/watch?v=ki36iUBbCDY&list=PLE2kqXhbJ00hLE8XAe4c204Cr4RJtHGv-&index=14&t=93s
+// JS code based on YouTube tutorial https://www.youtube.com/watch?v=ki36iUBbCDY&list=PLE2kqXhbJ00hLE8XAe4c204Cr4RJtHGv-&index=14&t=93s
 
-var crystalRed = 0;
-var crystalGreen = 0;
-var crystalBlue = 0;
-var crystalPurple = 0;
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Object/sub-objects to track value of crystals
+    
+    var crystal = {
+        red:
+        {
+            //name: "Red",
+            value: 0
+        },
+        green:
+        {
+            //name: "Green"
+            value: 0
+        },
+        blue:
+        {
+            //name: "Blue"
+            value: 0
+        },
+        purple:
+        {
+            //name: "Purple"
+            value: 0
+        }
+    };
+
+    var scoreTally = 0; //Running total of user score
+    var targetScore = 0; //Randomly generated score for user to try and meet
+    var wins = 0;
+    var losses = 0;
+
+
+    var gameStart = function() {
+        
+    }
+
+
+
+
+
+    $("#crystal0").click(function() {
+        alert("test0");
+    })
+
+    $("#crystal1").click(function() {
+        alert("test1");
+    })
+
+    $("#crystal2").click(function() {
+        alert("test2");
+    })
+
+    $("#crystal3").click(function() {
+        alert("test3");
+    })
 
 
 
@@ -12,22 +64,5 @@ var crystalPurple = 0;
 
 
 
+})
 
-
-
-
-
-
-
-
-
-
-
-
-// Function to generate random number  
-//function randomNumber(min, max) {  
-    //return Math.floor(Math.random() * (max - min) + min); 
-//}
-  
-// Function call 
-//$("#random-num-gen").html("<h2>" + ( randomNumber(19, 120) + "</h2>");
